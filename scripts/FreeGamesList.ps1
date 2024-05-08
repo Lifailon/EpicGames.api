2ee75696ee43f8fe0fcd09b7ac5f5498677e4425
Lifailon <deadepicboss@gmail.com>
2024-05-09 01:56:55 +0300

    update request

--- scripts/FreeGamesList.ps1
+++ scripts/FreeGamesList.ps1
@@ -3,16 +3,17 @@ $count = "500"
 $price = "tierFree"
 $url = "https://store.epicgames.com/ru/browse?sortBy=releaseDate&sortDir=DESC&priceTier=$($price)&category=Game&count=$($count)&start=0"
 # Делаем запрос
-$Version = Get-Random -Minimum 10 -Maximum 124
-$userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/$($Version).0.0.0 Safari/537.36"
-$handler = New-Object System.Net.Http.HttpClientHandler
-$handler.AllowAutoRedirect = $true
-$httpClient = New-Object System.Net.Http.HttpClient($handler)
-$requestMessage = [System.Net.Http.HttpRequestMessage]::new([System.Net.Http.HttpMethod]::Get, $url)
-$requestMessage.Headers.Add("User-Agent", $userAgent)
-$requestMessage.Headers.Add("Accept", "text/html")
-$response = $httpClient.SendAsync($requestMessage).Result
-$content = $response.Content.ReadAsStringAsync().Result
+$session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+$session.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
+$response = Invoke-WebRequest -UseBasicParsing -Uri $url -WebSession $session -Headers @{
+    "Accept"="text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
+    "Service-Worker-Navigation-Preload"="true"
+    "Upgrade-Insecure-Requests"="1"
+    "sec-ch-ua"="`"Chromium`";v=`"124`", `"Google Chrome`";v=`"124`", `"Not-A.Brand`";v=`"99`""
+    "sec-ch-ua-mobile"="?0"
+    "sec-ch-ua-platform"="`"Windows`""
+}
+$content = $response.Content
 # Логируем (проверяем содержимое)
 Write-Host "---------------------- Agent ------------------------"
 Write-Host $userAgent
