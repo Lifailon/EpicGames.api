4cb1efd3c1050b862242fd1107937927d8e50c2e
Lifailon <deadepicboss@gmail.com>
2024-05-09 02:04:02 +0300

    update request

--- scripts/FreeGamesList.ps1
+++ scripts/FreeGamesList.ps1
@@ -3,6 +3,26 @@ $count = "500"
 $price = "tierFree"
 $url = "https://store.epicgames.com/ru/browse?sortBy=releaseDate&sortDir=DESC&priceTier=$($price)&category=Game&count=$($count)&start=0"
 # Делаем запрос
+# $Agents = $(
+#     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+#     "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0",
+#     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.818.66 Safari/537.36 Edg/90.0.818.46",
+#     "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko",
+#     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36 OPR/60.0.3255.170",
+#     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36 Brave/90.0",
+#     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36 Vivaldi/3.7"
+# )
+# $userAgent = Get-Random -InputObject $Agents
+# $handler = New-Object System.Net.Http.HttpClientHandler
+# $handler.AllowAutoRedirect = $true
+# $httpClient = New-Object System.Net.Http.HttpClient($handler)
+# # WebClient
+# $requestMessage = [System.Net.Http.HttpRequestMessage]::new([System.Net.Http.HttpMethod]::Get, $url)
+# $requestMessage.Headers.Add("User-Agent", $userAgent)
+# $requestMessage.Headers.Add("Accept", "text/html")
+# $response = $httpClient.SendAsync($requestMessage).Result
+# $content = $response.Content.ReadAsStringAsync().Result
+
 $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
 $session.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
 $response = Invoke-WebRequest -UseBasicParsing -Uri $url -WebSession $session -Headers @{
@@ -14,6 +34,7 @@ $response = Invoke-WebRequest -UseBasicParsing -Uri $url -WebSession $session -H
     "sec-ch-ua-platform"="`"Windows`""
 }
 $content = $response.Content
+
 # Логируем (проверяем содержимое)
 Write-Host "---------------------- Agent ------------------------"
 Write-Host $userAgent
