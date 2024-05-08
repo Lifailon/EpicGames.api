fa485cde43f7f67b3d5f873028c59ce295f52579
Lifailon <deadepicboss@gmail.com>
2024-05-08 21:44:23 +0300

    added json file generation with free games

--- Scripts/FreeGamesList.ps1
+++ Scripts/FreeGamesList.ps1
@@ -1,12 +1,17 @@
 $count = "500"
 $price = "tierFree"
 $url = "https://store.epicgames.com/ru/browse?sortBy=releaseDate&sortDir=DESC&priceTier=$($price)&category=Game&count=$($count)&start=0"
-$headers = @{
-    "User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
-    "Accept" = "text/html"
-}
-$response = Invoke-WebRequest -Uri $url -Headers $headers -Method Get
-$json = $($($($response.Content -split "__REACT_QUERY_INITIAL_QUERIES__ = ")[1] -split "window.server_rendered")[0] -replace ";")
+
+$userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
+$httpClient = New-Object System.Net.Http.HttpClient
+$requestMessage = [System.Net.Http.HttpRequestMessage]::new([System.Net.Http.HttpMethod]::Get, $url)
+$requestMessage.Headers.Add("User-Agent", $userAgent)
+$requestMessage.Headers.Add("Accept", "text/html")
+$response = $httpClient.SendAsync($requestMessage).Result
+$content = $response.Content.ReadAsStringAsync().Result
+
+$json = $($($($content -split "__REACT_QUERY_INITIAL_QUERIES__ = ")[1] -split "window.server_rendered")[0] -replace ";")
 $games = $($json | ConvertFrom-Json).queries.state.data[-1].catalog.searchStore.elements
-$path = "$PWD\freeGames.json"
-$games | Out-File $path
+
+$path = "${env:GITHUB_WORKSPACE}/freeGames.json"
+$games | ConvertTo-Json -Depth 10 | Out-File $path
